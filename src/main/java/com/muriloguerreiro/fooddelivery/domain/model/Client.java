55a9f8426c0819a835991c1c884cce3093f5e37a
Murilo Guerreiro <guerreiro_murilo@hotmail.com>
2021-04-10 20:49:40 -0300

    Adding Jakarta Persistence to map classes

--- src/main/java/com/muriloguerreiro/fooddelivery/domain/model/Client.java
+++ src/main/java/com/muriloguerreiro/fooddelivery/domain/model/Client.java
@@ -1,8 +1,17 @@
 package com.muriloguerreiro.fooddelivery.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String document;
 	private String phone;
@@ -51,6 +60,31 @@ public class Client {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
